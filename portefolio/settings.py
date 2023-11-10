362c779204ef51a83d6c03aa2c0b553ff5ebacd5
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-11-10 15:26:25 +0100

    database

--- portefolio/settings.py
+++ portefolio/settings.py
@@ -83,10 +83,12 @@ WSGI_APPLICATION = 'portefolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'NAME': BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'deterministic': True,
+        },
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
